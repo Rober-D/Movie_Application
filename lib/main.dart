d533d14eba36af1ed3d5c6ac436b0adf250f8ad5
Rober-D <Roberdanial06@gmail.com>
2024-03-06 21:16:02 +0200

    start movie app

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp();
+}
+
+class Movie extends StatelessWidget {
+  const Movie({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+    );
+  }
+}
